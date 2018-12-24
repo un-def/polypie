63e9ad96d2d2cc7cab9b6de0cbefa1841146058d
un.def <un.def@ya.ru>
2018-12-24 20:55:19 +0300

    Remove redundant signature check

--- polypie.py
+++ polypie.py
@@ -1,6 +1,6 @@
 from collections import OrderedDict
 from functools import update_wrapper
-from inspect import Signature, signature, Parameter
+from inspect import Parameter, signature
 from typing import get_type_hints
 
 from typeguard import check_argument_types, _CallMemo
@@ -36,18 +36,16 @@ class HashableParameter(Parameter):
 def _call_func(func_key, args=None, kwargs=None):
     args = args or ()
     kwargs = kwargs or {}
-    for parameters_tuple, func in _registry[func_key].items():
+    for func in _registry[func_key].values():
         try:
-            Signature(parameters_tuple).bind(*args, **kwargs)
             check_argument_types(_CallMemo(func, args=args, kwargs=kwargs))
         except TypeError:
             continue
         return func(*args, **kwargs)
     raise PolypieException(
         "Matсhing signature for function '{func}' with "
-        "args={args} and kwargs={kwargs} not found".format(func=func_key,
-                                                           args=args,
-                                                           kwargs=kwargs)
+        "args={args} and kwargs={kwargs} not found".format(
+            func=func_key, args=args, kwargs=kwargs)
     )
 
 
@@ -73,17 +71,17 @@ def polymorphic(func):
         signature_mapping.dispatcher = dispatcher
         _registry[func_key] = signature_mapping
         return dispatcher
+    elif parameters_tuple not in _registry[func_key]:
+        _registry[func_key][parameters_tuple] = func
+        dispatcher = _registry[func_key].dispatcher
+        return update_dispatcher(dispatcher, func, assign=False)
     else:
-        if parameters_tuple in _registry[func_key]:
-            hints = get_type_hints(func)
-            sig_gen = ('{}:{}'.format(p, hints[p]) if p in hints else p
-                       for p in parameters)
-            raise PolypieException(
-                "Function '{func}' with signature ({sig}) "
-                "already exists".format(func=func_key,
-                                        sig=', '.join(sig_gen))
-            )
-        else:
-            _registry[func_key][parameters_tuple] = func
-            dispatcher = _registry[func_key].dispatcher
-            return update_dispatcher(dispatcher, func, assign=False)
+        hints = get_type_hints(func)
+        sig_gen = (
+            '{}:{}'.format(p, hints[p]) if p in hints else p
+            for p in parameters
+        )
+        raise PolypieException(
+            "Function '{func}' with signature ({sig}) "
+            "already exists".format(func=func_key, sig=', '.join(sig_gen))
+        )
